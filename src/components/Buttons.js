d1428af06ec919601890cb6a232f32c5fd80cf3f
npiriou <n.piriou@yahoo.fr>
2021-01-24 13:14:54 +0100

    added icons to most buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,25 +1,47 @@
+import { useState, useEffect } from "react";
+
 import Shop from "./Shop.js";
+import "./Buttons.css";
+import { ReactComponent as StartButton } from "../pictures/misc/swords-emblem(1).svg";
+import { ReactComponent as StartButtonHover } from "../pictures/misc/swords-emblem(2).svg";
+import { ReactComponent as StartButtonDisabled } from "../pictures/misc/swords-emblem.svg";
 
 function Buttons(props) {
-  const workerPrice = 10;
-  const clickHandlerWorker = () => {
-    if (props.gold >= 5) {
-      props.passWorkers(props.nbWorkers + 1);
-      props.passGold(props.gold - workerPrice);
-    }
-  };
+  const [startButtonHovered, setStartButtonHovered] = useState(false);
+  const startButtonDisabled =
+    (props.boardCards.frontLine.length === 0 ||
+      (props.boardCards.frontLine.length < 4 &&
+        props.boardCards.backLine.length > 0)) &&
+    props.inShop;
+
   const clickHandlerStartRound = () => {
     props.passInShop(false);
   };
-
+  const startButtonHover = () => {
+    setStartButtonHovered(true);
+  };
+  const startButtonLeft = () => {
+    setStartButtonHovered(false);
+  };
   return props.inShop ? (
     <div className="Buttons">
-      <button id="start-button" onClick={clickHandlerStartRound}>
-        Start round
+      <button
+        id="start-button"
+        className={startButtonDisabled ? "disabled" : "enabled"}
+        onClick={clickHandlerStartRound}
+        onMouseOver={startButtonHover}
+        onMouseLeave={startButtonLeft}
+        disabled={startButtonDisabled}
+      >
+        {startButtonDisabled ? (
+          <StartButtonDisabled />
+        ) : startButtonHovered ? (
+          <StartButtonHover />
+        ) : (
+          <StartButton />
+        )}
       </button>
-      <button onClick={clickHandlerWorker} disabled={props.gold < workerPrice}>
-        {workerPrice}G Buy a worker
-      </button>
+
       <Shop
         shopCards={props.shopCards}
         passShopCards={props.passShopCards}
@@ -27,6 +49,8 @@ function Buttons(props) {
         passBoardCards={props.passBoardCards}
         gold={props.gold}
         passGold={props.passGold}
+        passWorkers={props.passWorkers}
+        nbWorkers={props.nbWorkers}
       />
     </div>
   ) : null;
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,25 +1,26 @@
-import React from "react";
+import { useState } from "react";
 import Modal from "react-modal";
 import Card from "./Card.js";
 import Score from "./Score.js";
 import "./Shop.css";
+import { ReactComponent as ShopLogo } from "../pictures/misc/cash.svg";
+import { ReactComponent as ShopLogoHover } from "../pictures/misc/cash(1).svg";
+import { ReactComponent as BuyWorker } from "../pictures/misc/farmer.svg";
+import { ReactComponent as BuyWorkerHover } from "../pictures/misc/farmer(1).svg";
+import { ReactComponent as BuyWorkerDisabled } from "../pictures/misc/farmer(2).svg";
 
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
 // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
 //Modal.setAppElement("#yourAppElement");
 
 function Shop(props) {
-  //var subtitle;
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const workerPrice = 10;
+  const maxWorkers = 10;
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [shopButtonHovered, setShopButtonHovered] = useState(false);
+  const [workerButtonHovered, setWorkerButtonHovered] = useState(false);
+  const workerButtonDisabled =
+    props.gold < workerPrice || props.nbWorkers >= maxWorkers;
+
   function openModal() {
     setIsOpen(true);
   }
@@ -32,10 +33,34 @@ function Shop(props) {
   function closeModal() {
     setIsOpen(false);
   }
-
+  const shopButtonHover = () => {
+    setShopButtonHovered(true);
+  };
+  const shopButtonLeft = () => {
+    setShopButtonHovered(false);
+  };
+  const clickHandlerWorker = () => {
+    if (props.gold >= 5) {
+      props.passWorkers(props.nbWorkers + 1);
+      props.passGold(props.gold - workerPrice);
+    }
+  };
+  const workerButtonHover = () => {
+    setWorkerButtonHovered(true);
+  };
+  const workerButtonLeft = () => {
+    setWorkerButtonHovered(false);
+  };
   return (
     <div>
-      <button onClick={openModal}>SHOP</button>
+      <button
+        onClick={openModal}
+        id="openShopButton"
+        onMouseOver={shopButtonHover}
+        onMouseLeave={shopButtonLeft}
+      >
+        {shopButtonHovered ? <ShopLogoHover /> : <ShopLogo />}
+      </button>
       <Modal
         isOpen={modalIsOpen}
         onAfterOpen={afterOpenModal}
@@ -63,6 +88,25 @@ function Shop(props) {
               <div className="shop-card-price">{`${card.tier * 5} gold`}</div>
             </div>
           ))}
+          <div>
+            <button
+              id="buyWorkerButton"
+              onClick={clickHandlerWorker}
+              onMouseOver={workerButtonHover}
+              onMouseLeave={workerButtonLeft}
+              disabled={workerButtonDisabled}
+              className={workerButtonDisabled ? "disabled" : "enabled"}
+            >
+              {workerButtonDisabled ? (
+                <BuyWorkerDisabled />
+              ) : workerButtonHovered ? (
+                <BuyWorkerHover />
+              ) : (
+                <BuyWorker />
+              )}
+            </button>
+            <div className="shop-card-price">{`${workerPrice} gold`}</div>
+          </div>
         </div>
       </Modal>
     </div>
